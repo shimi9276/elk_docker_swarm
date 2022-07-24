2050547386f147c86d2f768add5f8ff6f75cf97b
shimi9276 <shimi_wieder@Shimis-MacBook-Pro.local>
2022-07-24 19:56:49 +0300

    init commit

---
+++ basic_installer.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+sudo apt-get update 
+
+sudo apt install docker.io -y  && sudo apt install docker-compose -y
+
+sudo groupadd docker
+
+sudo usermod -aG docker $USER
+
+sudo sysctl -w vm.max_map_count=262144
+
+echo "vm.max_map_count=262144" | sudo tee -a /etc/sysctl.conf > /dev/null
+
---
+++ ingress.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+yes | sudo docker network rm ingress 
+
+sleep 3
+
+sudo docker network create --driver overlay --ingress --subnet=10.255.255.0/24 ingress
+
+sudo systemctl restart docker
